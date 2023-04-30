076ac0ac204cec3c2180a4d943d806a57a4b93a3
ItzVladik <vladchuov636@gmail.com>
2023-04-30 14:39:51 +0500

    Update Script

--- mod.sh
+++ mod.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-for file in $(find android/src/me/nillerusr/ -name '*.java')
-	do sed -i -r 's/modname/'$MODNAME'/g' $file
+for file in $(find android/ -name '*.java' )
+	do 
+		sed -i -r 's/modname/'$MODNAME'/g' $file; 
 done
 
-for file in $(find android/res/values/ -name '*.xml')
-	do sed -i -r 's/modnamestring/'$MODNAMESTRING'/g' $file
-done
-
-for file in $(find android/ -name '*.xml')
-	do sed -i -r 's/modname/'$MODNAME'/g' $file
+for file in $(find android/ -name '*.xml' )
+	do
+		sed -i -r 's/namemod/'$MODNAMESTRING'/g' $file;
 done
