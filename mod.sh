bd6ef87c9a71d054d2daf8f01d8a95b4a4c51c2d
ItzVlλdik <61982094+ItzVladik@users.noreply.github.com>
2023-05-02 18:08:23 +0500

    Update mod.sh

--- mod.sh
+++ mod.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
+#!/bin/bash
 for file in $(find android/ -name '*.java' )
 	do 
 		sed -i -r 's/modname/'$MODNAME'/g' $file; 
 done
 
-for file in $(find android/ -name '*.xml' )
-	do
-		sed -i -r 's/namemod/'$MODNAMESTRING'/g' $file;
-done
+sed -i -r 's/namemod/'$MODNAMESTRING'/g' 'android/res/values/updates.xml';
+sed -i -r 's/modname/'$MODNAME'/g' 'android/AndroidManifest.xml';
